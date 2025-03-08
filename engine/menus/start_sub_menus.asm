24364b4a5e1b3e19ead3b42b0f6672cf210e8715
CreamElDudJafar <jahern2398@gmail.com>
2025-03-08 10:50:32 -0600

    Hopefully Fix Port PC Text Crashing in Elite 4

--- engine/menus/start_sub_menus.asm
+++ engine/menus/start_sub_menus.asm
@@ -824,6 +824,15 @@ SwitchPartyMon_InitVarOrSwapData:
 	ret
 
 StartMenu_PortablePC:: ; new
+
+; if none of the above cp is met, let's open the pc and do the things
+; next piece is to preserve the map text pointers
+	ld hl, wCurMapTextPtr
+	ld a, [hli]
+	ld [wUniQuizAnswer], a
+	ld a, [hl]
+	ld [wUniQuizAnswer+1], a
+
 	ld a, [wCurMap] ; we don't want to cheese the Elite4, do we?
 	cp LORELEIS_ROOM
 	jr z, .cantUseItHere
@@ -843,11 +852,12 @@ StartMenu_PortablePC:: ; new
 ; normal stuff
 	callfar ActivatePC ; main part
 	jr .done
+
 .cantUseItHere ; no cheese!
 	ld hl, CantUsePCHere
 	rst _PrintText
 .done
-	; next piece is to preserve the map text pointers
+; next piece is to preserve the map text pointers
 	push hl
 	ld hl, wUniQuizAnswer
 	ld a, [hli]
@@ -860,6 +870,7 @@ StartMenu_PortablePC:: ; new
 	call LoadTextBoxTilePatterns
 	call UpdateSprites
 	jp RedisplayStartMenu
+
 CantUsePCHere:
 	text_far _CantUsePCHere
 	text_end
