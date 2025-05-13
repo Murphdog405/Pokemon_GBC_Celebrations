d042c970e59a5fe92360033c3f0204c88585ff76
CreamElDudJafar <jahern2398@gmail.com>
2025-05-13 10:04:47 -0500

    Fast Heal Now
    After Healing First Time, Shorter time, shorter messages.

--- engine/events/pokecenter.asm
+++ engine/events/pokecenter.asm
@@ -1,5 +1,7 @@
 DisplayPokemonCenterDialogue_::
 	call SaveScreenTilesToBuffer1 ; save screen
+	CheckEvent EVENT_FIRST_POKECENTER
+	jr nz, .skiptext1
 	ld hl, PokemonCenterWelcomeText
 	rst _PrintText
 	ld hl, wd72e
@@ -14,10 +16,19 @@ DisplayPokemonCenterDialogue_::
 	ld a, [wCurrentMenuItem]
 	and a
 	jr nz, .declinedHealing ; if the player chose No
+	jp .skipHealingText
+.skiptext1
+	ld hl, ShallWeHealYourPokemonFastText
+	rst _PrintText
+.skipHealingText
 	call SetLastBlackoutMap
 	call LoadScreenTilesFromBuffer1 ; restore screen
+	call LoadCurrentMapView
+	CheckEvent EVENT_FIRST_POKECENTER
+	jr nz, .skiptext2
 	ld hl, NeedYourPokemonText
 	rst _PrintText
+.skiptext2
 	ld a, $18
 	ld [wSprite01StateData1ImageIndex], a ; make the nurse turn to face the machine
 	call Delay3
@@ -31,8 +42,11 @@ DisplayPokemonCenterDialogue_::
 	ld [wLastMusicSoundID], a
 ;	ld [wNewSoundID], a
 	call PlayMusic
+	CheckEvent EVENT_FIRST_POKECENTER
+	jr nz, .FightingFitShort
 	ld hl, PokemonFightingFitText
 	rst _PrintText
+.FightingFitShort
 	ld a, $14
 	ld [wSprite01StateData1ImageIndex], a ; make the nurse bow
 	ld c, a
@@ -40,9 +54,14 @@ DisplayPokemonCenterDialogue_::
 	jr .done
 .declinedHealing
 	call LoadScreenTilesFromBuffer1 ; restore screen
+	jp .skipEventFirstPokecenter
 .done
+	SetEvent EVENT_FIRST_POKECENTER
+.skipEventFirstPokecenter
 	ld hl, PokemonCenterFarewellText
 	rst _PrintText
+	ld a, PLAYER_DIR_DOWN
+	ld [wPlayerMovingDirection], a
 	jp UpdateSprites
 
 PokemonCenterWelcomeText:
@@ -54,6 +73,11 @@ ShallWeHealYourPokemonText:
 	text_far _ShallWeHealYourPokemonText
 	text_end
 
+ShallWeHealYourPokemonFastText:
+	text_far _ShallWeHealYourPokemonText
+	text_pause
+	text_end
+
 NeedYourPokemonText:
 	text_far _NeedYourPokemonText
 	text_end
@@ -63,6 +87,6 @@ PokemonFightingFitText:
 	text_end
 
 PokemonCenterFarewellText:
-	text_pause
+;	text_pause
 	text_far _PokemonCenterFarewellText
 	text_end
