40ecd8ae6bded629e04f28827e113cfed7774595
CreamElDudJafar <jahern2398@gmail.com>
2025-06-06 18:46:41 -0500

    Added Cheat Code File To Repo
    These cheats will work in game

---
+++ CHEAT CODES IN GAME.asm
@@ -0,0 +1,20 @@
+CHEATS:
+
+THE FOLLOWING CHEATS WILL WORK IN GAME: DISABLE ONCE CHEAT IS DONE BEING USED...... USE AT OWN RISK. MAY CAUSE OTHER BUGS/GLITCHES.
+
+
+Infinite Money:
+
+019948D3 ; After done disable it. Noticed weird bugs in graphics when enabled
+
+Walk Through Wall:
+
+010138CD
+
+Rare Candies in Mart:
+
+0128E3D0
+
+Master Balls in Mart:
+
+0101E3D0
