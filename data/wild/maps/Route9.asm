0b20954cb45cc1aa5bcd0636b6469a611c9dd78a
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-06-27 10:47:45 -0700

    Update Route9.asm

--- data/wild/maps/Route9.asm
+++ data/wild/maps/Route9.asm
@@ -2,24 +2,24 @@ Route9WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 16, RATTATA
 	db 16, SPEAROW
-	db 14, VENONAT
+	db 14, RATTATA
 IF (DEF(_RED) || DEF(_GREEN)) 
-	db 11, EKANS
-	db 13, SPEAROW
 	db 15, EKANS
+	db 13, SPEAROW
+	db 11, SANDSHREW
 	db 17, RATTATA
 	db 17, SPEAROW
-	db 13, LICKITUNG
-	db 17, LICKITUNG
+	db 17, EKANS
+	db 13, SANDSHREW
 ENDC
 IF DEF(_BLUE)
-	db 11, SANDSHREW
-	db 13, SPEAROW
 	db 15, SANDSHREW
+	db 13, SPEAROW
+	db 11, EKANS
 	db 17, RATTATA
 	db 17, SPEAROW
-	db 13, LICKITUNG
-	db 17, LICKITUNG
+	db 17, SANDSHREW
+	db 13, EKANS
 ENDC
 	end_grass_wildmons
 
