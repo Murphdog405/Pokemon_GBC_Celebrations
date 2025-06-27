7ee32467d0f363d9e16910d02e0e561b2789e745
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-06-27 10:51:29 -0700

    Update SeafoamIslands1F.asm

--- data/wild/maps/SeafoamIslands1F.asm
+++ data/wild/maps/SeafoamIslands1F.asm
@@ -2,26 +2,26 @@ SeafoamIslands1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 30, SEEL
 IF (DEF(_RED) || DEF(_GREEN)) 
-	db 30, SLOWPOKE
+        db 30, SLOWPOKE
 	db 30, SHELLDER
 	db 30, HORSEA
-	db 28, HORSEA
-	db 21, ZUBAT
+	db 28, KRABBY
 	db 29, GOLBAT
 	db 28, PSYDUCK
 	db 28, SHELLDER
-	db 38, GOLDUCK
+	db 30, KABUTO
+	db 30, OMANYTE
 ENDC
 IF DEF(_BLUE)
 	db 30, PSYDUCK
 	db 30, STARYU
 	db 30, KRABBY
 	db 28, HORSEA
-	db 21, ZUBAT
 	db 29, GOLBAT
 	db 28, SLOWPOKE
 	db 28, STARYU
-	db 38, SLOWBRO
+	db 30, OMANYTE
+	db 30, KABUTO
 ENDC
 	end_grass_wildmons
 
