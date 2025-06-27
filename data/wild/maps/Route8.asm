999e4addf79d7b59b1ccfb4a04df239284ec406e
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-06-27 10:46:50 -0700

    Update Route8.asm

--- data/wild/maps/Route8.asm
+++ data/wild/maps/Route8.asm
@@ -2,26 +2,26 @@ Route8WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 18, PIDGEY
 IF (DEF(_RED) || DEF(_GREEN)) 
-	db 18, MANKEY
+	db 20, MANKEY
 	db 17, EKANS
 	db 16, GROWLITHE
-	db 20, WEEPINBELL
-	db 20, MANKEY
+	db 20, PIDGEY
+	db 18, MEOWTH
 	db 19, EKANS
-	db 17, GROWLITHE
-	db 25, TANGELA
-	db 28, TANGELA
+	db 17, SANDSHREW
+	db 15, VULPIX
+	db  7, MEW
 ENDC
 IF DEF(_BLUE)
-	db 18, MEOWTH
+	db 20, MEOWTH
 	db 17, SANDSHREW
 	db 16, VULPIX
-	db 20, WEEPINBELL
-	db 20, MEOWTH
+	db 20, PIDGEY
+	db 18, MANKEY
 	db 19, SANDSHREW
-	db 17, VULPIX
-	db 25, TANGELA
-	db 28, TANGELA
+	db 17, EKANS
+	db 15, GROWLITHE
+	db  7, MEW
 ENDC
 	end_grass_wildmons
 
