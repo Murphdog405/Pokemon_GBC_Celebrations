5a01ab20447f7c114beecdc81501f263026993ac
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-06-27 00:07:12 -0400

    Update RockTunnel1F.asm

--- data/wild/maps/RockTunnel1F.asm
+++ data/wild/maps/RockTunnel1F.asm
@@ -1,15 +1,15 @@
 RockTunnel1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 16, ZUBAT
+	db 17, ZUBAT
 	db 17, GEODUDE
-	db 15, MACHOP	
-	db 18, CUBONE
-	db 22, MAGMAR
-	db 25, GRAVELER
-	db 25, GRAVELER
-	db 23, RHYHORN
+	db 15, MACHOP
+	db 16, GEODUDE
+	db 18, ZUBAT
+	db 15, ZUBAT
+	db 17, MACHOP
 	db 13, ONIX
-	db 18, ONIX
+	db 15, ONIX
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
