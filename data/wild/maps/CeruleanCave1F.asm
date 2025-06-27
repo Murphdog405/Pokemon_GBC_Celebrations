01ee1964e8036bdf5def7f239175b26d81a5858a
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-06-26 23:49:10 -0400

    Update CeruleanCave1F.asm

--- data/wild/maps/CeruleanCave1F.asm
+++ data/wild/maps/CeruleanCave1F.asm
@@ -1,20 +1,20 @@
 CeruleanCave1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
+	db 46, GOLBAT
 	db 46, HYPNO
 	db 46, MAGNETON
-	db 46, OMASTAR
-	db 49, KABUTOPS
-	db 49, RHYDON
+	db 49, DODRIO
+	db 49, VENOMOTH
 IF (DEF(_RED) || DEF(_GREEN)) 
-	db 52, PRIMEAPE
+	db 52, ARBOK
 ENDC
 IF DEF(_BLUE)
-	db 52, PRIMEAPE
+	db 52, SANDSLASH
 ENDC
 	db 49, KADABRA
-	db 52, HITMONLEE
+	db 52, PARASECT
 	db 53, RAICHU
-	db 53, ALAKAZAM
+	db 53, DITTO
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
