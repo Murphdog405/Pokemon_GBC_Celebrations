c28f27dd75a6ee8f003fdaa284b91eef3fd60224
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-06-27 00:10:41 -0400

    Update Route10.asm

--- data/wild/maps/Route10.asm
+++ data/wild/maps/Route10.asm
@@ -1,25 +1,25 @@
 Route10WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 16, VOLTORB
-	db 16, MAGNEMITE
-	db 14, KRABBY
+	db 16, SPEAROW
+	db 14, VOLTORB
 IF (DEF(_RED) || DEF(_GREEN)) 
-	db 11, EKANS
-	db 14, VULPIX
 	db 15, EKANS
+	db 13, SPEAROW
+	db 11, SANDSHREW
 	db 17, VOLTORB
-	db 18, MAGNEMITE
-	db 17, ABRA
-	db 20, POLIWAG
+	db 17, SPEAROW
+	db 17, EKANS
+	db 13, SANDSHREW
 ENDC
 IF DEF(_BLUE)
-	db 11, SANDSHREW
-	db 14, VULPIX
 	db 15, SANDSHREW
+	db 13, SPEAROW
+	db 11, EKANS
 	db 17, VOLTORB
-	db 18, MAGNEMITE
-	db 17, ABRA
-	db 20, POLIWAG
+	db 17, SPEAROW
+	db 17, SANDSHREW
+	db 13, EKANS
 ENDC
 	end_grass_wildmons
 
