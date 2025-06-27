c3732c8bd2fe1bd6b6726f7c50bd9b49a77c409a
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-06-27 10:45:03 -0700

    Update Route6.asm

--- data/wild/maps/Route6.asm
+++ data/wild/maps/Route6.asm
@@ -3,26 +3,26 @@ Route6WildMons:
 IF (DEF(_RED) || DEF(_GREEN)) 
 	db 13, ODDISH
 	db 13, PIDGEY
-	db 15, MEOWTH
-	db 10, MANKEY
-	db 16, STARYU
+	db 15, PIDGEY
+	db 12, MANKEY
+	db 10, MEOWTH
+	db 16, ODDISH
 	db 15, BELLSPROUT
-	db 18, POLIWHIRL
-	db 16, SLOWPOKE
-	db 14, SQUIRTLE
-	db 14, SQUIRTLE
+	db 16, PIDGEY
+	db 16, MANKEY
+	db 14, MEOWTH
 ENDC
 IF DEF(_BLUE)
 	db 13, BELLSPROUT
 	db 13, PIDGEY
-	db 15, MANKEY
-	db 10, MEOWTH
-	db 16, STARYU
+	db 15, PIDGEY
+	db 12, MEOWTH
+	db 10, MANKEY
+	db 16, BELLSPROUT
 	db 15, ODDISH
-	db 18, POLIWHIRL
-	db 16, SLOWPOKE
-	db 14, SQUIRTLE
-	db 14, SQUIRTLE
+	db 16, PIDGEY
+	db 16, MEOWTH
+	db 14, MANKEY
 ENDC
 	end_grass_wildmons
 
