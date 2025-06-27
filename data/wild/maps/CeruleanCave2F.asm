111f93b7cab8256ee4f105f5a329bbbbcb9f9d97
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-06-26 23:50:39 -0400

    Update CeruleanCave2F.asm

--- data/wild/maps/CeruleanCave2F.asm
+++ data/wild/maps/CeruleanCave2F.asm
@@ -1,15 +1,15 @@
 CeruleanCave2FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 51, MAROWAK
-	db 51, BLASTOISE
-	db 51, CHARIZARD
-	db 52, VENUSAUR
-	db 52, HITMONCHAN
-	db 52, AERODACTYL
-	db 56, PORYGON
+	db 51, DODRIO
+	db 51, VENOMOTH
+	db 51, KADABRA
+	db 52, RHYDON
+	db 52, MAROWAK
+	db 52, ELECTRODE
+	db 56, CHANSEY
 	db 54, WIGGLYTUFF
-	db 55, GENGAR
-	db 60, CHANSEY
+	db 55, DITTO
+	db 60, DITTO
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
