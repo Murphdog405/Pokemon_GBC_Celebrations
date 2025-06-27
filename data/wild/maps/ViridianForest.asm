8ac919754aef0a08b3e71ffb1fea7bb35598c37a
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-06-27 10:58:16 -0700

    Update ViridianForest.asm

--- data/wild/maps/ViridianForest.asm
+++ data/wild/maps/ViridianForest.asm
@@ -5,20 +5,20 @@ IF (DEF(_RED) || DEF(_GREEN))
 	db  5, KAKUNA
 	db  3, CATERPIE
 	db  5, WEEDLE
+	db  4, KAKUNA
+	db  6, KAKUNA
 	db  4, METAPOD
-	db  3, PINSIR
-	db  3, PINSIR
-	db  9, PIDGEOTTO
+	db  3, WEEDLE
 ENDC
 IF DEF(_BLUE)
 	db  4, CATERPIE
 	db  5, METAPOD
 	db  3, WEEDLE
 	db  5, CATERPIE
+	db  4, METAPOD
+	db  6, METAPOD
 	db  4, KAKUNA
-	db  3, PINSIR
-	db  3, PINSIR
-	db  9, PIDGEOTTO
+	db  3, CATERPIE
 ENDC
 	db  3, PIKACHU
 	db  5, PIKACHU
