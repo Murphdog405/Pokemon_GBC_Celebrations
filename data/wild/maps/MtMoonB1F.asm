f3f17fd08dd4399f6498cb306e5f7d3cccb1031a
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-06-26 23:55:15 -0400

    Update MtMoonB1F.asm

--- data/wild/maps/MtMoonB1F.asm
+++ data/wild/maps/MtMoonB1F.asm
@@ -3,10 +3,10 @@ MtMoonB1FWildMons:
 	db  8, ZUBAT
 	db  7, ZUBAT
 	db  7, GEODUDE
-	db  10, ONIX
+	db  8, GEODUDE
 	db  9, ZUBAT
 	db 10, PARAS
-	db 10, ONIX
+	db 10, ZUBAT
 	db 11, ZUBAT
 	db  9, CLEFAIRY
 	db  9, GEODUDE
