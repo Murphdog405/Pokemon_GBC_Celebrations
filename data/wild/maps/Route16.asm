de78ef9c6b9b93d785e75d6fb207a78478571e31
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-06-27 10:25:29 -0700

    Update Route16.asm

--- data/wild/maps/Route16.asm
+++ data/wild/maps/Route16.asm
@@ -1,15 +1,15 @@
 Route16WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 28, GRIMER
-	db 28, GRIMER
-	db 26, KOFFING
+	db 20, SPEAROW
+	db 22, SPEAROW
+	db 18, RATTATA
 	db 20, DODUO
-	db 25, KOFFING
+	db 20, RATTATA
 	db 18, DODUO
-	db 22, PONYTA
-	db 22, PONYTA
-	db 25, RATICATE
-	db 28, RATICATE
+	db 22, DODUO
+	db 22, RATTATA
+	db 23, RATICATE
+	db 25, EEVEE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
