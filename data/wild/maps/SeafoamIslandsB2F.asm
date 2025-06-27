ffdfe776e2a3256bcb9df699e0128ed595042a23
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-06-27 10:53:41 -0700

    Update SeafoamIslandsB2F.asm

--- data/wild/maps/SeafoamIslandsB2F.asm
+++ data/wild/maps/SeafoamIslandsB2F.asm
@@ -2,28 +2,26 @@ SeafoamIslandsB2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 	db 30, SEEL
 IF (DEF(_RED) || DEF(_GREEN)) 
+	db 32, SLOWPOKE
+	db 32, SEEL
+	db 30, PSYDUCK
+	db 30, HORSEA
 	db 30, STARYU
-	db 32, HORSEA
-	db 32, SHELLDER
-	db 30, KRABBY
-	db 30, SLOWPOKE
-	db 30, SEEL
-	db 28, PSYDUCK
-	db 28, SEEL
-	db 38, DEWGONG
-	db 37, SEADRA
+	db 28, KRABBY
+	db 28, SHELLDER
+	db 30, GOLBAT
+	db 37, SLOWBRO
 ENDC
 IF DEF(_BLUE)
+	db 32, PSYDUCK
+	db 32, SEEL
+	db 30, SLOWPOKE
+	db 30, KRABBY
 	db 30, SHELLDER
-	db 32, KRABBY
-	db 32, STARYU
-	db 30, HORSEA
-	db 30, PSYDUCK
-	db 30, SEEL
-	db 28, SLOWPOKE
-	db 28, SEEL
-	db 38, DEWGONG
-	db 37, KINGLER
+	db 28, HORSEA
+	db 28, STARYU
+	db 30, GOLBAT
+	db 37, GOLDUCK
 ENDC
 	end_grass_wildmons
 
