bf8121cfa7a0af399cba7bd58ef19b14ed8708b8
Murphdog405 <80846749+Murphdog405@users.noreply.github.com>
2025-06-27 10:48:52 -0700

    Update SafariZoneCenter.asm

--- data/wild/maps/SafariZoneCenter.asm
+++ data/wild/maps/SafariZoneCenter.asm
@@ -3,24 +3,24 @@ SafariZoneCenterWildMons:
 IF (DEF(_RED) || DEF(_GREEN)) 
 	db 22, NIDORAN_M
 	db 25, RHYHORN
-	db 22, PINSIR
+	db 22, VENONAT
 	db 24, EXEGGCUTE
 	db 31, NIDORINO
-	db 25, EXEGGCUTE
+	db 23, SCYTHER
 	db 31, NIDORINA
 	db 30, PARASECT
-	db 23, SCYTHER
+	db 23, PINSIR
 ENDC
 IF DEF(_BLUE)
 	db 22, NIDORAN_F
 	db 25, RHYHORN
-	db 22, SCYTHER
+	db 22, VENONAT
 	db 24, EXEGGCUTE
 	db 31, NIDORINA
-	db 25, EXEGGCUTE
+	db 23, PINSIR
 	db 31, NIDORINO
 	db 30, PARASECT
-	db 23, PINSIR
+	db 23, SCYTHER
 ENDC
 	db 23, CHANSEY
 	end_grass_wildmons
